c75a6be5ea433a1beb017f44fd5d79357d3af664
Adrian Hanson <hansonai@vm-infr-gw349.vampire>
2024-10-28 14:38:38 -0500

    initial commit

---
+++ basic_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash --norc
+
+#SBATCH --account=csb_gpu_acc
+#SBATCH --partition=turing
+#SBATCH --cpus-per-task=6
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=24G
+#SBATCH --time=00:20:00
+#SBATCH --job-name=output
+#SBATCH --output=output.log
+#SBATCH --error=error.err
+
+source ~/miniconda3/etc/profile.d/conda.sh   # Source the conda script (adjust path if needed)
+conda activate kinome  
+
+# Run your Python script
+python /dors/wankowicz_lab/adrian/kinase_colabfold/kinase_new_structs/DCGAN_server.py
