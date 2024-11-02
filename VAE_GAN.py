8373f9a712a22e053712048d6c8aba63b6ff808e
Adrian Hanson <33525795+Adronz@users.noreply.github.com>
2024-11-01 23:10:59 -0500

    Add files via upload

--- VAE_GAN.py
+++ VAE_GAN.py
@@ -1,18 +1,17 @@
 print("SCRIPT HAS STARTED RUNNING")
+
 import numpy as np 
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
-import csv
-import math
-import struct
-import random
+from torchsummary import summary
+from torch.optim.lr_scheduler import StepLR
 import matplotlib.pyplot as plt
-import struct
-from array import array
-from os.path  import join
 from torchvision.utils import save_image
+import time 
+
+start_time = time.time()
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
@@ -30,116 +29,43 @@ test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, trans
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
 
-def save_n_images(n, generator, file_dir):
+
+def save_n_images(n, generator, file_dir, real_data, latent_channels):
+    # Ensure the model uses its current device
+    device = next(generator.parameters()).device  
     generator.eval()
+
     if n > 64:
-        n=64
-        
+        n = 64
+
     batch_size = 64
 
-    noise = torch.randn(batch_size, input_dim, 1, 1, device=device)
+    # Create noise on the generator's device
     with torch.no_grad():
-        logits = generator(noise)
-    print(logits.shape)
-    logits = logits.view(batch_size,28,28)
-
+        logits, _, _ = generator(real_data)
+            
     for i in range(n):
-        file_name=f'{file_dir}/MNIST_gen_{i}.png'
-        img = logits[i].cpu()
+        file_name = f'{file_dir}/CIFAR10_real_{i}.png'
+        img = logits[i].cpu()  # Move the image to CPU for saving
         plt.figure()
         save_image(img, file_name)
 
-
-class VAE(nn.Module):
-    def __init__(self, conv_dim, latent_dim):
-        super().__init__()
-        self.latent_dim = latent_dim
-
-        self.encoder = nn.Sequential(
-                nn.Conv2d(3, conv_dim, kernel_size=3, stride=2, padding=1), # output size = conv_dim, 
-                nn.BatchNorm2d(conv_dim),
-                nn.ReLU(),
-
-                nn.Conv2d(conv_dim, conv_dim * 2, kernel_size=3, stride=2, padding=1),
-                nn.BatchNorm2d(conv_dim*2),
-                nn.ReLU(),
-
-                nn.Conv2d(conv_dim * 2, conv_dim * 4, kernel_size=3, stride=2, padding=1),
-                nn.BatchNorm2d(conv_dim*4),
-                nn.ReLU(),
-
-                nn.Flatten(), #make it so this can be split into mu and log_var
-
-        )
-
-        self.mu_mlp = nn.Linear(2048, 2048)
-        self.var_mlp = nn.Linear(2048, 2048)
-
-    def forward(self, x):
-        #batch size, h * w
-        encoding = self.encode(x)
-        mu, log_var = encoding
-        # print(f'decoded shape: {decoded.shape}')
-        z = self.reparameterize(mu, log_var)
-        
-        return mu, log_var, z
-
-    def encode(self, input):
-        encoding = self.encoder(input)
-        mu = self.mu_mlp(encoding)
-        log_var = self.var_mlp(encoding)
-
-        return mu, log_var
-    
-    def reparameterize(self, mu, log_var):
-        # Reparameterization trick:
-        # let z ~= q_psi(z|x)
-        # reparamterize z = g_psi(x, epsilon)
-        # z = mu_i + sig_i * epsilon
+    for i in range(n):
+        file_name = f'{file_dir}/CIFAR10_sample_{i}.png'
+        img = generator.sample(batch_size, latent_channels, device)
 
 
-        sig = torch.exp(log_var * 0.5)
-        # print(sig)
-        
-        epsilon = torch.randn_like(sig)  # Random noise from a standard normal distribution.
-        z = mu + torch.mul(sig, epsilon)
-
-        return z
-    
-    def loss_function(self, output, target, mu, log_var, epoch):
-        #here is the bread and butter
-        #were looking to optimize mu and sigma
-        
-        recon_loss = F.binary_cross_entropy(output, target, reduction='sum')
-
-        KL_div = -0.5 * torch.sum(1 + log_var - mu**2 - torch.exp(log_var))
-
-        # print(f'kl div: {KL_div}')
-        beta = 4.0
-        kl_weight = min(1.0, epoch / 20.0)  # Gradually increase beta over the first 10 epochs
-        kl_effective = beta * KL_div
-        loss = kl_effective + recon_loss
-        
-
-        return loss, kl_effective, recon_loss
-
-    def sample(self):
-
-        sampled_z = torch.randn(self.latent_dim)
-
-        sampled_images = self.decoder(sampled_z)
-
-        return sampled_images
+    print('Saved Images!')
 
 
 class VAE_Generator(nn.Module):
-    def __init__(self, conv_dim, input_dim, output_1, output_2, output_3, image_channels):
+    def __init__(self, conv_dim, output_1, output_2, output_3, image_channels):
         super().__init__()
         #* important note: the input channels, and the input are different. the layer only cares about the # of channels, not batch size, HxW, etc
         
         
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, conv_dim, kernel_size=3, stride=2, padding=1), # output size = conv_dim, 
+            nn.Conv2d(3, conv_dim, kernel_size=3, stride=2, padding=1), 
             nn.BatchNorm2d(conv_dim),
             nn.ReLU(),
 
@@ -150,50 +76,61 @@ class VAE_Generator(nn.Module):
             nn.Conv2d(conv_dim * 2, conv_dim * 4, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(conv_dim*4),
             nn.ReLU(),
-
-            nn.Flatten(), #make it so this can be split into mu and log_var
-
         )
 
         self.mu_mlp = nn.Linear(2048, 2048) #! not sure about this layer dimension
-        self.var_mlp = nn.Linear(2048, 2048)
+        self.log_var_mlp = nn.Linear(2048, 2048)
         
-        
+        #* important note: the input channels, and the input are different. the layer only cares about the # of channels, not batch size, HxW, etc
+        #*4x2x1 doubles the image size 
+    
         self.generator = nn.Sequential(
+
         #layer 1
-        nn.ConvTranspose2d(input_dim, output_1, kernel_size=4, stride=1), 
+        nn.ConvTranspose2d(conv_dim * 4, output_1, kernel_size=4, stride=2, padding=1), 
         nn.BatchNorm2d(output_1),
-        nn.ReLU(), # image size (output_1, 4, 4)
+        nn.ReLU(), # image size (output_1, 8, 8)
 
-        nn.ConvTranspose2d(output_1, output_2, kernel_size=3, stride=2, padding=1),
+        nn.ConvTranspose2d(output_1, output_2, kernel_size=4, stride=2, padding=1),
         nn.BatchNorm2d(output_2),
-        nn.ReLU(), #image size(output_2, 7, 7)
+        nn.ReLU(), #image size(output_2, 16, 16)
 
-        nn.ConvTranspose2d(output_2, output_3, kernel_size=4, stride=2, padding=1), #image size (output_3, 14, 14)
-        nn.BatchNorm2d(output_3), 
-        nn.ReLU(),
+        #*put this back if you are going up to 64x64
+        # nn.ConvTranspose2d(output_2, output_3, kernel_size=4, stride=2, padding=1), #image size (output_3, 32, 32)
+        # nn.BatchNorm2d(output_3), 
+        # nn.ReLU(),
 
-        nn.ConvTranspose2d(output_3, image_channels, kernel_size=4, stride=2, padding=1), #image size (image_channels, 28, 28)
-        nn.Tanh() #4x2x1 doubles the image size 
+        nn.ConvTranspose2d(output_2, image_channels, kernel_size=4, stride=2, padding=1), #image size (image_channels, 28, 28)
+        nn.Tanh() 
         )
         
     def forward(self, x):
-        encoding = self.encode(x)
-        mu, log_var = encoding
-        # print(f'decoded shape: {decoded.shape}')
+        batch_size = x.shape[0]
+
+        #get trainable layers
+        mu, log_var = self.encode(x)
+
+        #reparamiterization trick to allow effective sampling
         z = self.reparameterize(mu, log_var)
+
+        #resize z to be the shape before it was flattened
+        z = z.view(batch_size, 128, 4, 4) 
 
         generation = self.generator(z)
 
-        return generation
+        return generation, mu, log_var
     
     #generator loss fxn
-    def loss_function(self, D_g, output, target, mu, log_var, epoch):
+    def loss_function(self, D_g, mu, log_var, real_lth_layer, fake_lth_layer, epoch):
 
         #* ENCODER LOSS
-        #! replace recon loss with gaussian similarity 
-        disc_l_loss = 0
-        recon_loss = F.binary_cross_entropy(output, target, reduction='sum')
+
+        lth_layer_sq_diff = (real_lth_layer - fake_lth_layer)**2
+        log_prob_dx = -0.5 * lth_layer_sq_diff.sum(1) #The derivative of ln(x) = 1/x. 
+        disc_l_loss = -log_prob_dx.mean()
+
+        
+        # recon_loss = F.binary_cross_entropy(output, target, reduction='sum')
 
         KL_div = -0.5 * torch.sum(1 + log_var - mu**2 - torch.exp(log_var))
 
@@ -201,7 +138,7 @@ class VAE_Generator(nn.Module):
         beta = 4.0
         kl_weight = min(1.0, epoch / 20.0)  # Gradually increase beta over the first 10 epochs
         kl_effective = beta * KL_div
-        vae_loss = kl_effective + recon_loss
+        vae_loss = kl_effective
 
         #* GENERATOR LOSS
         epsilon = 1e-8
@@ -215,14 +152,21 @@ class VAE_Generator(nn.Module):
         
         #* TOTAL LOSS
 
-        total_loss = vae_loss + gan_loss
+        total_loss = vae_loss + gan_loss + disc_l_loss
 
         return total_loss
     
     def encode(self, input):
+        # print(f"Input shape to encoder: {input.shape}")  # Debug statement
+
         encoding = self.encoder(input)
+
+        # print(f'encoding shape is {encoding.shape}')
+
+        #flatten encoding
+        encoding = encoding.view(input.shape[0], -1)
         mu = self.mu_mlp(encoding)
-        log_var = self.var_mlp(encoding)
+        log_var = self.log_var_mlp(encoding)
 
         return mu, log_var
     
@@ -241,11 +185,11 @@ class VAE_Generator(nn.Module):
 
         return z
     
-    def sample(self):
+    def sample(self,batch_size, latent_channels, device):
 
-        sampled_z = torch.randn(self.latent_dim)
+        sampled_z = torch.randn(batch_size, latent_channels, 4, 4, device=device)
 
-        sampled_images = self.decoder(sampled_z)
+        sampled_images = self.generator(sampled_z)
 
         return sampled_images
 
@@ -253,43 +197,51 @@ class VAE_Generator(nn.Module):
         
 
 class DC_Discriminator(nn.Module):
-    def __init__(self, output_1, output_2, output_3, image_channels):
+    def __init__(self, conv_dim, image_channels):
         super().__init__()
 
-        self.discriminator = nn.Sequential(
-                nn.Conv2d(image_channels, output_3, kernel_size=4, stride=2, padding=1), 
-                nn.BatchNorm2d(output_3), 
-                nn.LeakyReLU(0.2),
+        self.conv1 = nn.Conv2d(image_channels, conv_dim, kernel_size=4, stride=2, padding=1)
+        self.bn1 = nn.BatchNorm2d(conv_dim)
+        
+        self.conv2 = nn.Conv2d(conv_dim, conv_dim * 2, kernel_size=4, stride=2, padding=1)
+        self.bn2 = nn.BatchNorm2d(conv_dim * 2)
+        
+        self.conv3 = nn.Conv2d(conv_dim * 2, conv_dim * 4, kernel_size=3, stride=2, padding=1)
+        self.bn3 = nn.BatchNorm2d(conv_dim * 4)
+        
+        # self.conv4 = nn.Conv2d(conv_dim * 4, 1, kernel_size=3, stride=2, padding=1)
+        
+        self.fc = nn.Linear(conv_dim * 4 * 4 * 4, 1)
 
-                nn.Conv2d(output_3, output_2, kernel_size=4, stride=2, padding=1), 
-                nn.BatchNorm2d(output_2), 
-                nn.LeakyReLU(0.2),
-
-                nn.Conv2d(output_2, output_1, kernel_size=3, stride=2, padding=1),
-                nn.BatchNorm2d(output_1), 
-                nn.LeakyReLU(0.2),
-
-                nn.Conv2d(output_1, 1, kernel_size=3, stride=2, padding=1),
-                nn.Flatten(),
-
-                nn.Linear(16, 1),
-                nn.Sigmoid()
-        )
+        # Sigmoid activation for final output
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.leaky_relu(x, 0.2)
 
-        discrimination = self.discriminator(x)
-        print(discrimination.shape)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = F.leaky_relu(x, 0.2)
 
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = F.leaky_relu(x, 0.2)
 
+        # x = self.conv4(x)
+        x = x.view(x.shape[0], -1)
+        # print(f'shape into fc of discriminator {x.shape}')
 
-        return discrimination
+        lth_layer = x.clone()
+        
+        x = self.fc(x)
+        x = self.sigmoid(x)
 
-    def loss_function(self, D_g, D_x):
+        return x, lth_layer
+
+    def loss_function(self, D_g, D_x, D_s):
             epsilon = 1e-8
-
-            #maximize the probability that the data is real
-            # loss = -torch.mean(torch.log(D_x + epsilon) + torch.log(1 - D_g + epsilon)) #negative sign because we are doing gradient ASCENT
 
             #*CHANGING TO BCE LOSS
             fake_target = torch.zeros_like(D_g) + 0.05
@@ -297,29 +249,36 @@ class DC_Discriminator(nn.Module):
 
             fake_loss = F.binary_cross_entropy(D_g, fake_target) #we want D_g to go to zero
             real_loss = F.binary_cross_entropy(D_x, real_target) #we want D_x to go to one
+            sampled_loss = F.binary_cross_entropy(D_s, real_target) #we want D_x to go to one
 
-            loss = fake_loss + real_loss
+            loss = fake_loss + real_loss + sampled_loss
 
             return loss 
 
 
 
 # Example usage
-input_dim = 100  # Latent vector size
 output_1 = 128
 output_2 = 64
 output_3 = 32
-image_channels = 1  # For MNIST, which is grayscale
+image_channels = 3  # For MNIST, which is grayscale
+conv_dim = 32
+latent_channels = 128
 
-generator = VAE_Generator(32, input_dim, output_1, output_2, output_3, image_channels).to(device)
-discriminator = DC_Discriminator(output_1, output_2, output_3, image_channels).to(device)
+generator = VAE_Generator(conv_dim, output_1, output_2, output_3, image_channels).to(device)
+discriminator = DC_Discriminator(conv_dim, image_channels).to(device)
+
+
+print('generator')
+print(summary(generator, input_size=(3, 32, 32)))
+print(discriminator)
+print(summary(discriminator, input_size=(3, 32, 32)))
         
 
-from torch.optim.lr_scheduler import StepLR
 
 
 disc_lr = 1e-3
-gen_lr = 1e-4
+gen_lr = 1e-3
 
 gen_optimizer = torch.optim.Adam(generator.parameters(), lr= gen_lr)
 disc_optimizer = torch.optim.Adam(discriminator.parameters(), lr= disc_lr)
@@ -333,8 +292,9 @@ disc_trn_loss = []
 gen_val_loss = []
 disc_val_loss = [] 
 
-EPOCHS = 1
+EPOCHS = 2
 k = 1
+
 
 for epoch in range(EPOCHS):
     gen_trn_run = 0.0
@@ -347,34 +307,40 @@ for epoch in range(EPOCHS):
             discriminator.train()
             generator.train()
             gen_optimizer.zero_grad()  
-
             disc_optimizer.zero_grad()
 
             batch_size = train_data.shape[0]
 
-            real_data = train_data.view(batch_size, 1, 28, 28).to(device)
+            # Data handling
+            real_data = train_data.view(batch_size, 3, 32, 32).to(device)
+            fake_data, mu, log_var = generator(real_data)
+            fake_data = fake_data.detach() #We dont want to keep the computation graph
+            sampled_data = generator.sample(batch_size, latent_channels, device).detach()
 
-            noise = torch.randn(batch_size, input_dim, 1, 1, device= device)
+            # Get discriminator probabilites per type of data
+            real_prob, real_lth_layer = discriminator(real_data) 
+            fake_prob, fake_lth_layer = discriminator(fake_data)
+            sampled_prob, _ = discriminator(sampled_data)
 
-            fake_data = generator(noise).detach()
-
-            real_prob = discriminator(real_data)
-            fake_prob = discriminator(fake_data)
-
-            disc_loss = discriminator.loss_function(fake_prob, real_prob) 
-            gen_loss = generator.loss_function(fake_prob)
-
+            # Train the discriminator 
+            disc_loss = discriminator.loss_function(fake_prob, real_prob, sampled_prob) 
+            gen_loss = generator.loss_function(fake_prob, mu, log_var, real_lth_layer, fake_lth_layer, epoch)
 
             disc_loss.backward()
             disc_optimizer.step()
 
             
             if batch_idx % k == 0:
-                noise = torch.randn(batch_size, input_dim, 1, 1, device=device)
-                fake_data = generator(noise)
-                real_prob = discriminator(real_data)
-                fake_prob = discriminator(fake_data)
-                gen_loss = generator.loss_function(fake_prob)
+
+                #regenerate samples
+                fake_data, mu, log_var = generator(real_data)
+                
+                #regenerate probabilities
+                real_prob, real_lth_layer = discriminator(real_data) 
+                fake_prob, fake_lth_layer = discriminator(fake_data)
+
+                #train generator 
+                gen_loss = generator.loss_function(fake_prob, mu, log_var, real_lth_layer, fake_lth_layer, epoch)
 
                 gen_loss.backward()
                 gen_optimizer.step()
@@ -392,5 +358,12 @@ print("training finished!")
 
 
 
-save_n_images(10, generator, '/dors/wankowicz_lab/adrian/kinase_colabfold/kinase_new_structs/temp_outputs')
+data_iter = iter(test_loader)
+images, labels = next(data_iter)
+images = images.to(device)
 
+base_path = '/dors/wankowicz_lab/adrian/kinase_colabfold/kinase_new_structs/temp_outputs'
+save_n_images(10, generator, base_path, images ,latent_channels)
+
+elapsed_time = time.time() - start_time
+print(f'Program took {elapsed_time / 60} minutes to run')
