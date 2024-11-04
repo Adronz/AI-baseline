59db56c1f3646dc6ca7cd4444f94b6b79a03d6fb
Adrian Hanson <hansonai@vm-infr-gw348.vampire>
2024-11-03 19:28:15 -0600

    atari player initial commit

---
+++ atari_player.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import matplotlib.pyplot
+import numpy 
+import gymnasium as gym 
+from collections import deque
+
+
+
+#TODO function that takes observation and turns it into input state (downsampling, cropping, etc)
+def preprocess_obs():
+    return
+
+#TODO make memory recall that can be added to, kicks stuff when it gets too big, and uniformly sampled from 
+def memory_recall():
+    return
+
+#TODO network architecture 
+class Atari_Agent(nn.Module):
+    def __init__():
+        super.__init__()
+
+
